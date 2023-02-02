07b1ace30c343b922248b9151bf3e44fb8c59123
PakeezaFatma <you@example.com>
2023-02-02 11:42:31 +0530

    LocationOfExpenditur

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,13 +1,22 @@
-
+import './ExpenseItem.css';
 
 function ExpenseItem()
 {
-    return<div>
-        <h2> Expense Item!</h2>
-        <li> Food Rs 10</li>
-        <li> Petrol Rs 100</li>
-        <li> Movies Rs 200</li>
-    </div> 
+    const expenseDate = new Date(2023,2,2);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67
+    const expenditure='Delhi'
+    return (
+        <div className="expense-item">
+            <div>{expenseDate.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">{expenseAmount}</div>
+                <div className='expense-item__price'>{expenditure}</div>
+            </div>
+           
+        </div>
+    );
     
 }
 export default ExpenseItem;

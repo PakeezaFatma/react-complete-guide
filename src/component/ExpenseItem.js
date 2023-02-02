3ba0fcb2b5143ff1e66b19e9c8cf767487d7460a
PakeezaFatma <you@example.com>
2023-02-02 16:28:14 +0530

    UsingForLoopMap

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,18 +1,15 @@
 import './ExpenseItem.css';
 
-function ExpenseItem()
+function ExpenseItem(props)
 {
-    const expenseDate = new Date(2023,2,2);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67
-    const expenditure='Delhi'
+    
     return (
         <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">{expenseAmount}</div>
-                <div className='expense-item__price'>{expenditure}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">$ {props.amount}</div>
+                <div className='expense-item__price'>{props.expenditure}</div>
             </div>
            
         </div>

4408f3237c0dba7e4995419aaa5ff6c2bfdf4199
PakeezaFatma <you@example.com>
2023-02-09 12:25:23 +0530

    addButton

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,23 +1,19 @@
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetail from "./ExpenseDetail";
+import Card from "./Card";
+import "./ExpenseItem.css";
 
-import ExpenseDate from './ExpenseDate';
-import ExpenseDetail from './ExpenseDetail';
-import Card from './Card';
-import './ExpenseItem.css';
-
-function ExpenseItem(props)
-{
-    
-    return (
-        <Card className="expense-item">
-            
-          <ExpenseDate date={props.date}></ExpenseDate>
-          <ExpenseDetail title ={props.title} amount ={props.amount}/>
-         
-        
-           
-           
-        </Card>
-    );
-    
+const ExpenseItem =(props)  =>{
+  const clickHandler =()=>{
+    console.log('Cliked!!!');
+  }
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}></ExpenseDate>
+      <ExpenseDetail title={props.title} amount={props.amount} />
+      <button onClick={clickHandler}>Change Title</button>
+      <button >Delete</button>
+    </Card>
+  );
 }
 export default ExpenseItem;
--- src/component/Expenses.js
+++ src/component/Expenses.js
@@ -2,8 +2,10 @@ import ExpenseItem from "./ExpenseItem";
 import Card from './Card';
 import './Expenses.css';
  function Expenses(props){
-    
+  
     return (
+      
+     
         <Card className='expenses' >
     {
         props.items.map(expense =>(

e794a5ffd30db75ed2de4610355c1b0b793a8b0c
PakeezaFatma <you@example.com>
2023-02-10 16:39:28 +0530

    AddButtonAmount

--- src/component/ExpenseItem.js
+++ src/component/ExpenseItem.js
@@ -1,18 +1,23 @@
+import React, {useState} from "react";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetail from "./ExpenseDetail";
 import Card from "./Card";
 import "./ExpenseItem.css";
-
 const ExpenseItem =(props)  =>{
+ const [title, setTitle] = useState(props.title);
+ const [amount, setAmount] = useState(props.amount);
+
   const clickHandler =()=>{
-    console.log('Cliked!!!');
+    setTitle('updated');
+    setAmount('100$');
+    console.log(title);
   }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
-      <ExpenseDetail title={props.title} amount={props.amount} />
+      <ExpenseDetail title={title} amount={amount} />
       <button onClick={clickHandler}>Change Title</button>
-      <button >Delete</button>
+      <button onClick={clickHandler}>change Amount</button>
     </Card>
   );
 }

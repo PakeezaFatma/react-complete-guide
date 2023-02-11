2edcdee927bbf201446aeeb846d7f6e4b0362535
PakeezaFatma <you@example.com>
2023-02-11 20:38:38 +0530

    workingSubmitButton

--- src/component/NewExpense/ExpenseForm.js
+++ src/component/NewExpense/ExpenseForm.js
@@ -2,39 +2,55 @@ import React,{useState} from 'react'
 import './ExpenseForm.css'
 
 const ExpenseForm =() => {
-    // const [enteredTitle,setEnteredTitle] =useState('');
-    // const [enterAmount,setEnterAmount] =useState('');
-    // const [enterDate,setEnterDate] = useState('');
+    const [enteredTitle,setEnteredTitle] =useState('');
+    const [enterAmount,setEnterAmount] =useState('');
+    const [enterDate,setEnterDate] = useState('');
     //we can single usestate
-   const [userInput , setUserInput] = useState({
-        enterdTitle:'',
-        enterAmount:'',
-        enterDate:''
-    });
+//    const [userInput , setUserInput] = useState({
+//         enterdTitle:'',
+//         enterAmount:'',
+//         enterDate:''
+//     });
     const titleChangeHandler =(event) =>{
-       // setEnteredTitle(event.target.value);
-       setUserInput({
-        ...userInput, //beecause copy of rest value of object
-        enterdTitle:event.target.value,
-       })
+       setEnteredTitle(event.target.value);
+    //    setUserInput({
+    //     ...userInput, //beecause copy of rest value of object
+    //     enterdTitle:event.target.value,
+    //    })
+
+
+    //this is safer way as compare to above that code
+    // setUserInput((prevState) =>{
+    //     return {...prevState,enterdTitle:event.target.value};
+    // })
     };
     const amountChangeHandler =(event) =>{
-        //setEnterAmount(event.target.value);
+        setEnterAmount(event.target.value);
 
-        setUserInput({
-            ...userInput,
-            enterAmount:event.target.value,
-        })
+        // setUserInput({
+        //     ...userInput,
+        //     enterAmount:event.target.value,
+        // })
     }
     const dateChangeHandler =(event) =>{
-       // setEnterDate(event.target.value);
-       setUserInput({
-        ...userInput,
-        enterDate:event.target.value,
-       })
-    }
+       setEnterDate(event.target.value);
+    //    setUserInput({
+    //     ...userInput,
+    //     enterDate:event.target.value,
+    //    })
+    };
+
+    const submitHandler =(event) =>{
+    event.preventDefault();
+    const exepnseData ={
+        title:enteredTitle,
+        amount: enterAmount,
+        date: new Date(enterDate)
+    };
+    console.log(exepnseData);
+    };
     return (
-    <form>
+    <form onSubmit={submitHandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
@@ -49,7 +65,7 @@ const ExpenseForm =() => {
                 <input type='date' min="2019-01-01" max="2023-12-31" onChange={dateChangeHandler}/>
             </div>
             <div className='new-expense__actions'>
-                <button type='submit'>Add Expense</button>
+                <button type='submit' >Add Expense</button>
             </div>
         </div>
     </form>

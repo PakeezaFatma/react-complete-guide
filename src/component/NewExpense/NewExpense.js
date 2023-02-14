a66b26f8ffc3c4356cd7634db9cd0d1c6ceac3a8
PakeezaFatma <you@example.com>
2023-02-14 17:01:25 +0530

    StateLiftingStoreDate

--- src/component/NewExpense/NewExpense.js
+++ src/component/NewExpense/NewExpense.js
@@ -2,12 +2,19 @@ import React from "react";
 import ExpenseForm from "./ExpenseForm";
 import './NewExpense.css';
 
-const NewExpense =()=>{
+const NewExpense =(props)=>{
+    const saveExpenseDataHandler =(enteredExpenseData) =>{
+        const exepnseData ={
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        };
+        props.onAddExpense(exepnseData);
+    }
     return (
     <div className="new-expense">
-       <ExpenseForm /> 
+       <ExpenseForm onSaveExepnseData={saveExpenseDataHandler} /> 
     </div>
-    )
+    );
 };
 
 export default NewExpense;
